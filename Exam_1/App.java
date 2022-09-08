011f4b30c4906cd346b55593fd0b153da761d870
Rovic Mathew Bagadiong <111488993+Rhovicx@users.noreply.github.com>
2022-09-08 14:33:24 +0800

    Create App.java

---
+++ Exam_1/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) {
+
+        student me = new student();
+
+        me.surname = "Bagadiong";
+        me.firstname = "Rovic Mathew";
+        me.middleInitial = 'Z';
+        me.dateOfBirth = "12/19/2002";
+        me.studentNumber = 2022102269;
+        me.studentEmailAddress = "bagadiongrz@students.national-u.edu.ph";
+        me.iAmAwesome = true;
+        me.saySurname();
+        me.sayfirstname();
+        me.saymiddleInitial();
+        me.saydateofbirth();
+        me.saystudentnum();
+        me.saystudentEmail();
+        me.sayiamasomel();
+      
+
+        
+    }
+}
+
+
